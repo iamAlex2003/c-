a7e4bd3b9f6c55900a24f9c41ac26140317d3c18
Harshal Patil <89332986+hp1004@users.noreply.github.com>
2022-10-04 10:30:15 +0530

    Update addition.cpp

--- addition.cpp
+++ addition.cpp
@@ -8,4 +8,11 @@ int main()
     c=a+b;
     cout<<"ADDITION = "<<c;
     return 0;
+    
+    int l,m,n;
+    cout<<"ENTER 2 NUMBERS "<<endl;
+    cin>>l>>m;
+    n=l+m;
+    cout<<"ADDITION = "<<n;
+    return 0;
 }
